8fb86c7c88fa57036e99193a3c9fdbf9d55afcf1
hamardikan <a.hamardikan@gmail.com>
2026-01-08 10:22:32 +0700

    Draw player ship as triangle sprite

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,6 +12,8 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+
     while True:
         log_state()
 
@@ -19,6 +22,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
