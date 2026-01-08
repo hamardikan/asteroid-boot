db4b8863b069fe1d7a0cea2275ef3606f7149128
hamardikan <a.hamardikan@gmail.com>
2026-01-08 13:54:15 +0700

    Add asteroids and asteroid field spawner

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -15,12 +17,16 @@ def main():
     # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    # Add all future Players to both groups (must be set before Player is created)
+    # Add all future instances to groups
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
-    # Create player (auto-added to groups by CircleShape)
+    # Create objects
     Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+    AsteroidField()
 
     while True:
         log_state()
